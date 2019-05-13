2046913fee5a739534bbb0ec8954c2bd09f13be2
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-13 15:23:17 +0100

    setup movement for player2

---
+++ Pong2D/Assets/Scripts/Player2Racket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Racket : MonoBehaviour
+{
+    public float movementSpeed = 250;
+    public float maxMovementSpeed = 500;
+    private void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical2");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveVertical) * movementSpeed;
+    }
+    
+}
---
+++ Pong2D/Assets/Scripts/WallTriggers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallTriggers : MonoBehaviour
+{
+    public Text playerScore;
+    public int score = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+            score++;
+            playerScore.text = score.ToString();
+    }
+
+}
