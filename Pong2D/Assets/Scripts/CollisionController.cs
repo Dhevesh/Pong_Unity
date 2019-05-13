d36bfe91cf38da72253c9046a4c2ff9756802341
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-13 15:42:38 +0100

    Ball movement with direction change implemented.

---
+++ Pong2D/Assets/Scripts/CollisionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+    public BallMovement ballMovement;
+
+    void BounceFromRacket(Collision2D c)
+    {
+        Vector3 ballPosition = this.transform.position;
+        Vector3 racketPosition = c.gameObject.transform.position;
+
+        float racketHeight = c.collider.bounds.size.y;
+
+        float x = 0;
+        if (c.gameObject.name == "Player1Racket")
+        {
+            x = 1;
+        }
+        else
+        {
+            x = -1;
+        }
+
+        float y = (ballPosition.y - racketPosition.y) / racketHeight;
+
+        this.ballMovement.IncreaseHitCounter();
+        this.ballMovement.MoveBall(new Vector2(x, y));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player1Racket" || collision.gameObject.name == "Player2Racket")
+        {
+            this.BounceFromRacket(collision);
+        }else if (collision.gameObject.name == "WallLeft")
+        {
+            Debug.Log("Left wall hit");
+        }else if (collision.gameObject.name == "WallRight")
+        {
+            Debug.Log("Right Wall hit");
+        }
+    }
+}
