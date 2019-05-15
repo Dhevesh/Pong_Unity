a883ed2b375c18f1d2b4a253e755457877894a6a
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-15 16:07:34 +0100

    adjustments to options menu.

---
+++ Pong2D/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void DifficultySelector()
+    {
+        string componentName;
+
+        componentName = this.GetComponent<Toggle>().name;
+
+        switch (componentName)
+        {
+            case "Toggle_Easy":
+                break;
+            case "Toggle_Medium":
+                break;
+            case "Toggle_Hard":
+                break;
+
+        }
+    }
+
+}
